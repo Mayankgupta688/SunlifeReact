f3e76e3a69e621be46c78ca9b323aa31ac931bcc
MayankGupta <mayank.gupta.6.88@gmail.com>
2022-07-01 12:56:19 +0530

    Session One

---
+++ JavaScript/GettingStarted/interprettedVsCompiled.js
@@ -0,0 +1,27 @@
+debugger;
+
+showData()
+
+console.log(username) // undefined
+
+console.log(userage)  // Error / undefined
+
+
+// Declaration and defination in single line
+var username = "Mayank"
+
+// Declaration
+var username
+
+// Definition
+username = "Mayank"
+
+var userage = 10
+
+function showData() {
+    console.log("123")
+}
+
+// Compiled: Before execution, construct of code in known
+
+// Interpretted: JavaScript is twice interpretted
---
+++ JavaScript/RandomNumber/index.js
@@ -0,0 +1,7 @@
+// Write a code to genarate a random number between 0 to 67
+
+var random = require('randomize');
+
+var randomNumber = random(67);
+
+debugger;
